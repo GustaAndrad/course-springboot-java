46098465d4603d72027bcf18853494e7158719c0
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-22 22:35:36 -0300

    Service layer, component registration

--- src/main/java/com/gusta/course/resources/UserResource.java
+++ src/main/java/com/gusta/course/resources/UserResource.java
@@ -1,22 +1,34 @@
 package com.gusta.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gusta.course.entities.User;
+import com.gusta.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
-		return ResponseEntity.ok().body(u); 
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);  
  	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 	
 }
---
+++ src/main/java/com/gusta/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.gusta.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gusta.course.entities.User;
+import com.gusta.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
