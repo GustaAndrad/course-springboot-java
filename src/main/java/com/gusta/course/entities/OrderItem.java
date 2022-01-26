e365ccccc580d7312f2b03d5e1b25497ef3e565c
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-26 10:06:54 -0300

    Subtotal & Total methods

--- src/main/java/com/gusta/course/entities/OrderItem.java
+++ src/main/java/com/gusta/course/entities/OrderItem.java
@@ -65,6 +65,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
