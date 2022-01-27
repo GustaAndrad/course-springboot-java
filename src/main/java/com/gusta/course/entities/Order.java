7dcdebd9a6378d2db782489ba015640765c93fc9
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-27 10:03:30 -0300

    Tratamento de exceçao - Delete

--- src/main/java/com/gusta/course/entities/Order.java
+++ src/main/java/com/gusta/course/entities/Order.java
@@ -100,11 +100,11 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-	
+
 	public Double getTotal() {
 		double sum = 0.0;
 		for (OrderItem x : items) {
-			sum +=  x.getSubTotal();
+			sum += x.getSubTotal();
 		}
 		return sum;
 	}
--- src/main/java/com/gusta/course/services/UserService.java
+++ src/main/java/com/gusta/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gusta.course.entities.User;
 import com.gusta.course.repositories.UserRepository;
+import com.gusta.course.services.exceptions.DataBaseException;
 import com.gusta.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
